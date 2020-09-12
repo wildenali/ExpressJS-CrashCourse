77206fce8774ba6ddd8f6f3894676be89de57130
wildenali <wildenwildenaliali@gmail.com>
2020-09-12 22:51:34 +0700

    Pindahin dummy data untuk API  ke file Members.js

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Members");
 
 const app = express();
 
-const members = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@gmail.com",
-    status: "active",
-  },
-  {
-    id: 2,
-    name: "Bob Williams",
-    email: "bob@gmail.com",
-    status: "inactive",
-  },
-  {
-    id: 3,
-    name: "Shannon Jackson",
-    email: "shannon@gmail.com",
-    status: "active",
-  },
-];
-
+// Gets All Members
 app.get("/api/members", (req, res) => {
   res.json(members);
 });
